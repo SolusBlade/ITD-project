ebf0d30614d9b80d7d0aa17d2bff37107682bb94
SolusBlade <121023802+SolusBlade@users.noreply.github.com>
2023-01-28 19:46:39 +0000

    Deploying to gh-pages from @ SolusBlade/ITD-project@a04016e6c4831953743908b9e202ff0393b7f0fd 🚀

---
+++ index.d8b23a84.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.d8b23a84.js.map

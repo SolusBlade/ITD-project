c1f3d0d126da94798e2c98093d6fedefc9793c9f
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-01-31 19:52:56 +0200

    add anchor links to header and footer

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,8 +1,8 @@
 (() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-  
+    const closeMenuBtn = document.querySelectorAll('.js-close-menu');
+  console.log(closeMenuBtn);
     const toggleMenu = () => {
       const isMenuOpen =
         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -16,7 +16,12 @@
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
+  // closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  closeMenuBtn.forEach(item => {
+    item.addEventListener('click', toggleMenu);
+  })
+
   
     // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
@@ -25,4 +30,5 @@
       openMenuBtn.setAttribute('aria-expanded', false);
       bodyScrollLock.enableBodyScroll(document.body);
     });
+  
   })();
